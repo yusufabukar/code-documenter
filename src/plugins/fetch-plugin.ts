8213f8efd5548bcfca641190ebda587197cfe9f9
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-10-04 14:38:34 +0100

    adding css loader workaround, splitting fetch plugin load functions by filters

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -7,34 +7,62 @@ const fileCache = localForage.createInstance({
 });
 
 const fetchPlugin = (inputCode: string) => {
-    return {
-        name: 'fetch-plugin',
-        setup(build: esbuild.PluginBuild) {
-            build.onLoad({filter: /.*/}, async (args: any) => {
-                if (args.path === 'index.js') {
-                    return {
-                        loader: 'jsx',
-                        contents: inputCode
-                    };
-                };
+	return {
+		name: 'fetch-plugin',
+		setup(build: esbuild.PluginBuild) {
+			build.onLoad({filter: /(^index\.js$)/}, () => {
+				return {
+					loader: 'jsx',
+					contents: inputCode
+				};
+			});
 
-                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-                if (cachedResult) {
-                    return cachedResult;
-                };
+			build.onLoad({filter: /.css$/}, async (args: any) => {
+				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+				if (cachedResult) {
+					return cachedResult;
+				};
 
-                const { data, request } = await axios.get(args.path);
-                const result: esbuild.OnLoadResult = {
-                    loader: 'jsx',
-                    contents: data,
-                    resolveDir: new URL('./', request.responseURL).pathname
-                };
-                await fileCache.setItem(args.path, result);
+				const { data, request }: { data: any, request: any } = await axios.get(args.path);
+				const escapedData = data
+					.replace(/\n/g, '')
+					.replace(/'/g, "\\'")
+					.replace(/"/g, '\\"');
+				const contents = `
+					const style = document.createElement('style');
+					style.innerText = '${escapedData}';
+					document.head.appendChild(style);
+				`;
 
-                return result;
-            });
-        }
-    };
+				const result: esbuild.OnLoadResult = {
+					loader: 'jsx',
+					contents,
+					resolveDir: new URL('./', request.responseURL).pathname
+				};
+				await fileCache.setItem(args.path, result);
+
+				return result;
+			});
+
+			build.onLoad({filter: /.*/}, async (args: any) => {
+				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+				if (cachedResult) {
+					return cachedResult;
+				};
+
+				const { data, request } = await axios.get(args.path);
+
+				const result: esbuild.OnLoadResult = {
+					loader: 'jsx',
+					contents: data,
+					resolveDir: new URL('./', request.responseURL).pathname
+				};
+				await fileCache.setItem(args.path, result);
+
+				return result;
+			});
+		}
+	};
 };
 
 export default fetchPlugin;
