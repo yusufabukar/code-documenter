9376443ec0214569d14568a9716f101feae7151e
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-10-11 16:02:03 +0100

    setting up prettier for formatting, format button, moving stuff to dev dependencies

--- src/components/codeEditor.tsx
+++ src/components/codeEditor.tsx
@@ -1,4 +1,7 @@
+import { useRef } from 'react';
 import Editor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
 interface CodeEditorProps {
 	initialValue: string;
@@ -6,27 +9,49 @@ interface CodeEditorProps {
 };
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+	const editorRef = useRef<any>();
+
 	const onEditorDidMount: EditorDidMount = (getValue, editor) => {
+		editorRef.current = editor;
 		editor.onDidChangeModelContent(() => onChange(getValue()));
 		editor.getModel()?.updateOptions({tabSize: 4});
 	};
 
-	return <Editor
-		editorDidMount={onEditorDidMount}
-		value={initialValue}
-		height='500px'
-		theme='dark'
-		language='javascript'
-		options={{
-			wordWrap: 'on',
-			minimap: {enabled: false},
-			showUnused: false,
-			folding: false,
-			lineNumbersMinChars: 3,
-			fontSize: 16,
-			scrollBeyondLastLine: false,
-			automaticLayout: true
-		}} />
+	const onFormatClick = () => {
+		const unformattedCode = editorRef.current.getModel().getValue();
+		const formattedCode = prettier.format(unformattedCode, {
+			parser: 'babel',
+			plugins: [parser],
+			useTabs: true,
+			singleQuote: true,
+			semi: true
+		});
+		
+		editorRef.current.setValue(formattedCode);
+	};
+
+	return (
+		<>
+		<button onClick={onFormatClick}>Format</button>
+		<Editor
+			editorDidMount={onEditorDidMount}
+			value={initialValue}
+			height='500px'
+			theme='dark'
+			language='javascript'
+			options={{
+				wordWrap: 'on',
+				minimap: {enabled: false},
+				showUnused: false,
+				folding: false,
+				lineNumbersMinChars: 3,
+				fontSize: 16,
+				scrollBeyondLastLine: false,
+				automaticLayout: true
+			}}
+		/>
+		</>
+	);
 };
 
 export default CodeEditor;
