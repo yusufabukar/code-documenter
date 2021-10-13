f4dc559326b183c4a48310edf97a9e9550c8f073
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-10-13 12:34:58 +0100

    moving to new machine temporarily

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import * as esbuild from 'esbuild-wasm';
-import unpkgPathPlugin from './plugins/unpkg-path-plugin';
-import fetchPlugin from './plugins/fetch-plugin';
+
 import CodeEditor from './components/codeEditor';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import Preview from './components/preview';
@@ -12,35 +10,14 @@ function App() {
 	const [ input, setInput ] = useState('');
 	const [ bundledCode, setBundledCode ] = useState('');
 
-	const startService = async () => {
-		ESBuildRef.current = await esbuild.startService({
-			worker: true,
-			wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-		});
-	};
-	
-	useEffect(() => {
-		startService();
-	}, []);
+
 
 	const onClick = async () => {
 		if (!ESBuildRef.current) {
 			return;
 		};
 
-		const bundle = await ESBuildRef.current.build({
-			entryPoints: ['index.js'],
-			bundle: true,
-			write: false,
-			plugins: [
-				unpkgPathPlugin(),
-				fetchPlugin(input)
-			],
-			define: {
-				global: 'window',
-				'process.env.NODE_ENV': '"production"'
-			}
-		});
+		const bundle = 
 
 		setBundledCode(bundle.outputFiles[0].text)
 	};
