b11a9846f035d563153477a6b48f3597cc39d505
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-10-25 11:32:00 +0100

    switching action types in reducer

--- src/redux/reducers/cellReducer.ts
+++ src/redux/reducers/cellReducer.ts
@@ -18,6 +18,19 @@ const initialState: CellState = {
     data: {}
 };
 
-const cellReducer = (state: CellState = initialState, action: Action): CellState => state;
+const cellReducer = (state: CellState = initialState, action: Action): CellState => {
+    switch (action.type) {
+        case ActionTypes.MOVE_CELL:
+            return state;
+        case ActionTypes.UPDATE_CELL:
+            return state;
+        case ActionTypes.DELETE_CELL:
+            return state;
+        case ActionTypes.INSERT_CELL_BEFORE:
+            return state;
+        default:
+            return state;
+    };
+};
 
 export default cellReducer;
