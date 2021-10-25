f09434028b5491bfeba6c646ac76963963485a86
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-10-25 12:25:48 +0100

    implementing update cell reducer

--- src/redux/reducers/cellReducer.ts
+++ src/redux/reducers/cellReducer.ts
@@ -3,34 +3,45 @@ import { ActionTypes } from '../actionTypes';
 import { Cell } from '../cell';
 
 interface CellState {
-    loading: boolean;
-    error: string | null;
-    order: string[];
-    data: {
-        [key: string]: Cell;
-    }
+	loading: boolean;
+	error: string | null;
+	order: string[];
+	data: {
+		[key: string]: Cell;
+	}
 };
 
 const initialState: CellState = {
-    loading: false,
-    error: null,
-    order: [],
-    data: {}
+	loading: false,
+	error: null,
+	order: [],
+	data: {}
 };
 
 const cellReducer = (state: CellState = initialState, action: Action): CellState => {
-    switch (action.type) {
-        case ActionTypes.MOVE_CELL:
-            return state;
-        case ActionTypes.UPDATE_CELL:
-            return state;
-        case ActionTypes.DELETE_CELL:
-            return state;
-        case ActionTypes.INSERT_CELL_BEFORE:
-            return state;
-        default:
-            return state;
-    };
+	switch (action.type) {
+		case ActionTypes.MOVE_CELL:
+			return state;
+		case ActionTypes.UPDATE_CELL:
+			const { id, content } = action.payload;
+
+			return {
+				...state,
+				data: {
+					...state.data,
+					[id]: {
+						...state.data[id],
+						content
+					}
+				}
+			};
+		case ActionTypes.DELETE_CELL:
+			return state;
+		case ActionTypes.INSERT_CELL_BEFORE:
+			return state;
+		default:
+			return state;
+	};
 };
 
 export default cellReducer;
