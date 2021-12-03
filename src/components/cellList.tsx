a225138092188009a0945f11eacb79f48cd030e9
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-12-03 21:27:55 +0000

    adding key prop to cell list item fragment

--- src/components/cellList.tsx
+++ src/components/cellList.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 import AddCell from './addCell';
 import CellListItem from './cellListItem';
@@ -5,10 +6,12 @@ import CellListItem from './cellListItem';
 const CellList: React.FC = () => {
 	const cells = useTypedSelector(({ cell: { order, data }}) => order.map(id => data[id]));
 
-	const renderedCells = cells.map(cell => <>
-		<AddCell nextCellID={cell.id} />
-		<CellListItem key={cell.id} cell={cell} />
-	</>);
+	const renderedCells = cells.map(cell => (
+		<Fragment key={cell.id}>
+			<AddCell nextCellID={cell.id} />
+			<CellListItem cell={cell} />
+		</Fragment>
+	));
 
 	return (
 		<div>
