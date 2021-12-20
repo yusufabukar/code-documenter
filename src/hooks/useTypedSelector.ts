9686a9e9062ee39600b6d6949b4baae567e6e8d7
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-12-20 15:07:00 +0000

    lerna setup

--- src/hooks/useTypedSelector.ts
+++
@@ -1,4 +0,0 @@
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
-import { RootState } from '../redux';
-
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
