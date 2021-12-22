46e1b30cb71ed72210537f65ff7bd91634ae3202
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-12-22 16:54:49 +0000

    linking client to server with express static and increasing client build maximum old space size to 4096MB

--- packages/local-server/dist/index.js
+++ packages/local-server/dist/index.js
@@ -4,15 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
+const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
-const http_proxy_middleware_1 = require("http-proxy-middleware");
 const serve = (filename, directory, port) => {
     const server = (0, express_1.default)();
-    server.use((0, http_proxy_middleware_1.createProxyMiddleware)({
-        target: 'http://localhost:3000',
-        ws: true,
-        logLevel: 'silent'
-    }));
+    const packagePath = require.resolve('local-client/build/index.html');
+    server.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    // server.use(createProxyMiddleware({
+    //     target: 'http://localhost:3000',
+    //     ws: true,
+    //     logLevel: 'silent'
+    // }));
     return new Promise((resolve, reject) => {
         server.listen(port, resolve).on('error', reject);
     });
--- packages/local-server/src/index.ts
+++ packages/local-server/src/index.ts
@@ -1,14 +1,18 @@
+import path from 'path';
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (filename: string, directory: string, port: number) => {
     const server = express();
 
-    server.use(createProxyMiddleware({
-        target: 'http://localhost:3000',
-        ws: true,
-        logLevel: 'silent'
-    }));
+    const packagePath = require.resolve('local-client/build/index.html');
+    server.use(express.static(path.dirname(packagePath)));
+
+    // server.use(createProxyMiddleware({
+    //     target: 'http://localhost:3000',
+    //     ws: true,
+    //     logLevel: 'silent'
+    // }));
 
     return new Promise<void>((resolve, reject) => {
         server.listen(port, resolve).on('error', reject)
