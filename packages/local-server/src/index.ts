ed250d5061d5053a3fe6f60400e2ed10f996805d
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-12-22 15:34:47 +0000

    creating proxy middleware between React application and local server

--- packages/local-server/src/index.ts
+++ packages/local-server/src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export const serve = (filename: string, directory: string, port: number) => {
     const server = express();
+
+    server.use(createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent'
+    }));
 
     return new Promise<void>((resolve, reject) => {
         server.listen(port, resolve).on('error', reject)
