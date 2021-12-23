dc98a550e865a0fba214afa40e36008427630de6
yusufabukar <65050809+yusufabukar@users.noreply.github.com>
2021-12-23 12:11:30 +0000

    creating fetchCells reducers

--- packages/local-client/src/redux/reducers/cellReducer.ts
+++ packages/local-client/src/redux/reducers/cellReducer.ts
@@ -32,17 +32,20 @@ const cellReducer = produce((state: CellState = initialState, action: Action): C
 			state.order[targetIndex] = action.payload.id;
 
 			return state;
+
 		case ActionTypes.UPDATE_CELL:
 			const { id, content } = action.payload;
 
 			state.data[id].content = content;
 
 			return state;
+
 		case ActionTypes.DELETE_CELL:
 			delete state.data[action.payload];
 			state.order = state.order.filter(id => id !== action.payload);
 
 			return state;
+
 		case ActionTypes.INSERT_CELL_AFTER:
 			const newCell: Cell = {
 				id: Math.random().toString(36).substr(2, 5),
@@ -60,6 +63,29 @@ const cellReducer = produce((state: CellState = initialState, action: Action): C
 			};
 
 			return state;
+
+		case ActionTypes.FETCH_CELLS_START:
+			state.loading = true;
+			state.error = null;
+
+			return state;
+
+		case ActionTypes.FETCH_CELLS_END:
+			state.order = action.payload.map(cell => cell.id);
+			state.data = action.payload.reduce((acc, cell) => {
+				acc[cell.id] = cell;
+
+				return acc;
+			}, {} as CellState['data']);
+
+			return state;
+		
+		case ActionTypes.FETCH_CELLS_ERROR:
+			state.loading = false;
+			state.error = action.payload;
+
+			return state;
+			
 		default:
 			return state;
 	};
